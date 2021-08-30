417dab33c25f6d7b1049992d78313bb060c1fd00
anait <a-blue@yandex.ru>
2021-08-30 14:45:00 +0300

    added new app products

--- products/views.py
+++ products/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# контроллеры = вьювс (вьюхи) = функции
+
+def index(request):
+    return render(request, 'products/index.html')
+
+
+def products(request):
+    return render(request, 'products/products.html')
+
