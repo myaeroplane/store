409a5d28e461b76f030cafdc91ae61d4900a3654
anait <a-blue@yandex.ru>
2021-08-31 12:32:02 +0300

    added new app products

--- products/views.py
+++ products/views.py
@@ -1,12 +1,50 @@
 from django.shortcuts import render
+from datetime import datetime
+
+current_date = datetime.now()
 
 # Create your views here.
 # контроллеры = вьювс (вьюхи) = функции
 
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'tittle': 'Store',
+        'current_date': current_date,
+    }
+    return render(request, 'products/index.html', context)
 
 
 def products(request):
-    return render(request, 'products/products.html')
+    context = {
+                  'tittle': 'Catalog',
+              }
+    return render(request, 'products/products.html', context)
 
+def test_context(request):
+    context = {
+        'tittle': 'тестовый контекст',
+        'head': 'Приветствую!',
+        'text': 'Привет, это моя тестовая страница',
+        'products': [
+            {
+                'name': 'Худи черного цвета с монограммами adidas Originals',
+                'price': 6053.00,
+            },
+            {
+                'name': 'Синяя куртка The North Face',
+                'price': 7045.00,
+            },
+            {
+                'name': 'Коричневый спортивный oversized-топ ASOS DESIGN',
+                'price': 4878.50,
+            },
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {
+                'name': 'Черный рюкзак Nike Heritage',
+                'price': 2036.00,
+            },
+        ],
+    }
+    return render(request, 'products/test_context.html', context)
